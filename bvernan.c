db2006faace7f903099835900ed84d02a43a9573
Urolit <aley.pallotta@gmail.com>
2021-07-11 17:58:04 +0200

    Start

---
+++ bvernan.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include "mycrypt.h"
+//#include libreria dei files
+
+int main(){
+    /*
+    1 verificare file chiave, file da criptare e file output se esiste, per l'output in caso si crea (qua potrebbe essere carino 
+     chiedere se salvare un'altro nuovo oppure sovrascrivere)
+    2 dal file chiave si prende la key 
+    3 il file di input viene preso a blocchi di key-byte e mandati al metodo di crypt
+    4 quello restituito dal metodo di crypt viene mandato in output
+    */
+
+}
+
+/*
+Libreria mycript.h
+
+metodo per criptare un blocco utilizzando una chiave,
+da ricordare che la key per l'ultimo blocco è più grande del blocco da cryptare
+*/
