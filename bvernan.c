57d8ff806f937c662a89098934be20ef71f06e8c
Urolit <aley.pallotta@gmail.com>
2021-07-18 15:44:08 +0200

    almost done
    cmon!!

--- bvernan.c
+++ bvernan.c
@@ -13,15 +13,15 @@ void main(int argc, char* k, char* i, char* o){
     3 il file di input viene preso a blocchi di key-byte e mandati al metodo di crypt
     4 quello restituito dal metodo di crypt viene mandato in output
     */
-    FILE *fdkey;
+    FILE *fpkey;
     long keylength;
     char* key;
 
-    FILE * fdinput;
+    FILE * fpinput;
     long inputlength;
     char* input;
 
-    FILE * fdoutput;
+    FILE * fpoutput;
     int nblocchi;
 
     /* Controllo input */
@@ -29,71 +29,42 @@ void main(int argc, char* k, char* i, char* o){
         printf("\n[0] Argomenti inseriti sono meno di 3!\n");
         exit(0);
     }
-    if(fdkey = fopen(k, "r") == NULL){
+    if(fpkey = fopen(k, "r") == NULL){
         printf("\n[1] Errore Apertura Key File!\n");
         exit(0);
     }
-    if(fdinput = fopen(i,"r") == NULL){
+    if(fpinput = fopen(i,"r") == NULL){
         printf("\n[1] Errore Apertura Input File!\n");
         exit(0);
     }
-    fdoutput = fopen(o, O_TRUNC|O_CREAT);
+    fpoutput = fopen(o, O_TRUNC|O_CREAT);
 
-    /* Misuro la grandezza del file e quindi della Key */
-    fseek( fdkey , 0L , SEEK_END);
-    keylength = ftell( fdkey );
-    rewind( fdkey );
+    /* Misuro la grandezza del file per la Key e lo alloco in memoria */
+    keylength = CalcolaLunghezza(fpkey);
+    key = LeggiFile(fpkey, keylength);
 
-    /* Alloco la memoria della chiave e leggo il key file */
-    key = calloc(1, keylength+1);
-    if(!key){
-        ChiudiTuttiFiles(fdkey, fdinput, fdoutput);
-        printf("\n[2] Errore allocazione memoria key!\n");
-        exit(0);
+    /* Misuro la grandezza del file in input e lo alloco in memoria */
+    inputlength = CalcolaLunghezza(fpinput);
+    input = LeggiFile(fpinput, inputlength);
+
+    /* Controllo che l'allocazione di key e input sia corretta */
+    if( key == NULL || input == NULL){
+        printf("\n[2] Errore Lettura Files!\n");
+        ChiudiTuttiFiles(fpkey, fpinput, fpoutput);
+        free(key);
+        free(input);
+        exit(1);
     }
     
-    if( fread(key, keylength, 1, fdkey =! 1)){
-        ChiudiTuttiFiles(fdkey, fdinput, fdoutput);
-        printf("\n[3] Errore lettura key file!\n");
-        exit(0);
-    }
+    /* Utilizzo la funzione Crypt per criptare o decriptare il file in input con la key 
+    allo stesso tempo scrivo il risultato con fputs nel file di output */
+    int risultato = fputs( Crypt(key, keylength, input, inputlength), fpoutput);
 
-    /* Misuro la grandezza del file input */
-    fseek( fdinput , 0L , SEEK_END);
-    inputlength = ftell( fdinput );
-    rewind( fdinput );
-
-    /* Leggo il file di input e lo alloco in memoria */
-    char ch;
-    int i = 0;
-    input = calloc(1, inputlength+1);
-    if(!input){
-        ChiudiTuttiFiles(fdkey, fdinput, fdoutput);
-        printf("\n[2] Errore allocazione memoria key!\n");
-        exit(0);
-    }
-    while((ch = fgetc(fdinput)) != EOF){    //TODO test se funziona senno copi quello della key, chissa perche lo hai fatto diverso x)
-      *(input+*i) = ch;
-      i++;
-    }
-
-    //TODO chiedere crypt o decrypt
-    int risultato = -1;
-    if(1){
-        risultato = fputs( Crypt(key, keylength, input, inputlength), fdoutput);
-    }else{
-        risultato = fputs( Decrypt(key, keylength, input, inputlength), fdoutput);
-    }
-    
-    if(risultato){
+    if(risultato != EOF){
         printf("\n[Ok] Operazione completata!\n");
-    }else{ printf("\n[Error] Operazione non eseguita correttamente :<\n");
-    }
+    }else printf("\n[Error] Operazione non eseguita correttamente :<\n");
 
-    ChiudiTuttiFiles(fdkey, fdinput, fdoutput);
-    //free(fdkey);
-    //free(fdinput);
-    //free(fdoutput);
+    ChiudiTuttiFiles(fpkey, fpinput, fpoutput);
     free(key);
     free(input);
 }
@@ -102,4 +73,29 @@ void ChiudiTuttiFiles(FILE * key, FILE * input, FILE * output ){
     fclose(key);
     fclose(input);
     fclose(output);
+}
+
+char* LeggiFile(FILE* f, int lungfile){
+    char* contenuto = calloc(1, lungfile+1);
+    if(!contenuto){
+        return NULL;
+    }
+    if( fread(contenuto, lungfile, 1, f =! 1)){
+        return NULL;
+    }
+    /* alternativa per la lettura
+    while((ch = fgetc(fpinput)) != EOF){
+      *(input+*i) = ch;
+      i++;
+    }
+    */
+    return contenuto;
+}
+
+int CalcolaLunghezza(FILE* f){
+    int lunghezza = 0;
+    fseek( f , 0L , SEEK_END);
+    lunghezza = ftell( f );
+    rewind( f );
+    return lunghezza;
 }
--- mycrypt.h
+++ mycrypt.h
@@ -1,9 +1,7 @@
-//da ricordare che la key per l'ultimo blocco è più grande del blocco da cryptare
-
 char* Crypt(char* key, long keylength, char* input, long inputlength){
     char toreturn[inputlength];
-    int navkey = 0; //navigatore della key, da 0 a k.length
-    int navblocchi = 0; //contatore dei blocchi j
+    int navkey = 0; //navigatore della key, da 0 a keyLength
+    int navblocchi = 0; //contatore dei blocchi creati da N/K
     for(int i = 0; i < inputlength; i++){
 
         toreturn[i] = input[i] ^ key[((i+navblocchi)%keylength)];
@@ -14,8 +12,4 @@ char* Crypt(char* key, long keylength, char* input, long inputlength){
         }else navkey++;
     }
     return toreturn;
-}
-
-char* Decrypt(char* key, long keylength, char* input, long inputlength){
-
 }
